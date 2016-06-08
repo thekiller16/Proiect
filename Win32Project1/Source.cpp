3c36ee4b0e16905cade6d3818d2de8633ef175df
thekiller16 <vladu1997@yahoo.com>
2016-06-09 00:53:58 +0300

    incercari 9.06

--- Win32Project1/Source.cpp
+++ Win32Project1/Source.cpp
@@ -155,7 +155,70 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR lpCmdLine, int nS
 
 	return 0;
 }
+LRESULT CALLBACK WinProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
 
+int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR lpCmdLine, int nShowCmd)
+{
+	WNDCLASSEX wClass;
+	ZeroMemory(&wClass, sizeof(WNDCLASSEX));
+	wClass.cbClsExtra = NULL;
+	wClass.cbSize = sizeof(WNDCLASSEX);
+	wClass.cbWndExtra = NULL;
+	wClass.hbrBackground = (HBRUSH)COLOR_WINDOW;
+	wClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wClass.hIcon = NULL;
+	wClass.hIconSm = NULL;
+	wClass.hInstance = hInst;
+	wClass.lpfnWndProc = (WNDPROC)WinProc;
+	wClass.lpszClassName = "Window Class";
+	wClass.lpszMenuName = NULL;
+	wClass.style = CS_HREDRAW | CS_VREDRAW;
+
+	if (!RegisterClassEx(&wClass))
+	{
+		int nResult = GetLastError();
+		MessageBox(NULL,
+			"Window class creation failed\r\n",
+			"Window Class Failed",
+			MB_ICONERROR);
+	}
+
+	HWND hWnd = CreateWindowEx(NULL,
+		"Window Class",
+		"Windows application",
+		WS_OVERLAPPEDWINDOW,
+		200,
+		200,
+		640,
+		480,
+		NULL,
+		NULL,
+		hInst,
+		NULL);
+
+	if (!hWnd)
+	{
+		int nResult = GetLastError();
+
+		MessageBox(NULL,
+			"Window creation failed\r\n",
+			"Window Creation Failed",
+			MB_ICONERROR);
+	}
+
+	ShowWindow(hWnd, nShowCmd);
+
+	MSG msg;
+	ZeroMemory(&msg, sizeof(MSG));
+
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return 0;
+}
 LRESULT CALLBACK WinProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
 	switch (msg)
