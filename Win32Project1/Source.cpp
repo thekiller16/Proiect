5020028c330065a46537dd8ae062e99eca9e554d
thekiller16 <vladu1997@yahoo.com>
2016-06-14 00:07:39 +0300

    14.6

--- Win32Project1/Source.cpp
+++ Win32Project1/Source.cpp
@@ -1,238 +1,84 @@
 #include <windows.h>
-#define IDC_MAIN_EDIT 101
-LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
-void AddMenus(HWND);
-
-#define IDM_FILE_NEW 1
-#define IDM_FILE_OPEN 2
-#define IDM_FILE_QUIT 3
-
-int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
-	PWSTR lpCmdLine, int nCmdShow) {
-
-	MSG  msg;
-	WNDCLASSW wc = { 0 };
-	wc.lpszClassName = L"Carte de telefon";
-	wc.hInstance = hInstance;
-	wc.hbrBackground = GetSysColorBrush(COLOR_3DFACE);
-	wc.lpfnWndProc = WndProc;
-	wc.hCursor = LoadCursor(0, IDC_ARROW);
-
-	RegisterClassW(&wc);
-	CreateWindowW(wc.lpszClassName, L"Carte de telefon",
-		WS_OVERLAPPEDWINDOW | WS_VISIBLE,
-		100, 100, 350, 250, 0, 0, hInstance, 0);
-
-	while (GetMessage(&msg, NULL, 0, 0)) {
-
-		TranslateMessage(&msg);
-		DispatchMessage(&msg);
-	}
-
-	return (int)msg.wParam;
-}
-
-LRESULT CALLBACK WndProc(HWND hwnd, UINT msg,
-	WPARAM wParam, LPARAM lParam) {
-
-	switch (msg) {
-
-	case WM_CREATE:
-
-		AddMenus(hwnd);
-		break;
-
-	case WM_COMMAND:
-
-		switch (LOWORD(wParam)) {
-
-		case IDM_FILE_NEW:
-		case IDM_FILE_OPEN:
-
-			MessageBeep(MB_ICONINFORMATION);
-			break;
-
-		case IDM_FILE_QUIT:
-
-			SendMessage(hwnd, WM_CLOSE, 0, 0);
-			break;
-		}
-
-		break;
-
-	case WM_DESTROY:
-
-		PostQuitMessage(0);
-		break;
-	}
-
-	return DefWindowProcW(hwnd, msg, wParam, lParam);
-}
-
-void AddMenus(HWND hwnd) {
-
-	HMENU hMenubar;
-	HMENU hMenu;
-
-	hMenubar = CreateMenu();
-	hMenu = CreateMenu();
-
-	AppendMenuW(hMenu, MF_STRING, IDM_FILE_NEW, L"&Introducere contact");
-	AppendMenuW(hMenu, MF_STRING, IDM_FILE_OPEN, L"&Modificare contact");
-	AppendMenuW(hMenu, MF_STRING, IDM_FILE_NEW, L"&Stergere contact");
-	AppendMenuW(hMenu, MF_STRING, IDM_FILE_OPEN, L"&Cautare contact");
-	AppendMenuW(hMenu, MF_SEPARATOR, 0, NULL);
-	AppendMenuW(hMenu, MF_STRING, IDM_FILE_QUIT, L"&Iesire");
-
-	AppendMenuW(hMenubar, MF_POPUP, (UINT_PTR)hMenu, L"&Optiuni");
-	SetMenu(hwnd, hMenubar);
-}
-#define IDC_MAIN_BUTTON	101			
-#define IDC_MAIN_EDIT	102			
-HWND hEdit;
-
-
-LRESULT CALLBACK WinProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
-
-int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR lpCmdLine, int nShowCmd)
-{
-	WNDCLASSEX wClass;
-	ZeroMemory(&wClass, sizeof(WNDCLASSEX));
-	wClass.cbClsExtra = NULL;
-	wClass.cbSize = sizeof(WNDCLASSEX);
-	wClass.cbWndExtra = NULL;
-	wClass.hbrBackground = (HBRUSH)COLOR_WINDOW;
-	wClass.hCursor = LoadCursor(NULL, IDC_ARROW);
-	wClass.hIcon = NULL;
-	wClass.hIconSm = NULL;
-	wClass.hInstance = hInst;
-	wClass.lpfnWndProc = (WNDPROC)WinProc;
-	wClass.lpszClassName = "Window Class";
-	wClass.lpszMenuName = NULL;
-	wClass.style = CS_HREDRAW | CS_VREDRAW;
-
-	if (!RegisterClassEx(&wClass))
-	{
-		int nResult = GetLastError();
-		MessageBox(NULL,
-			"Window class creation failed\r\n",
-			"Window Class Failed",
-			MB_ICONERROR);
-	}
-
-	HWND hWnd = CreateWindowEx(NULL,
-		"Window Class",
-		"Windows application",
-		WS_OVERLAPPEDWINDOW,
-		200,
-		200,
-		640,
-		480,
-		NULL,
-		NULL,
-		hInst,
-		NULL);
-
-	if (!hWnd)
-	{
-		int nResult = GetLastError();
-
-		MessageBox(NULL,
-			"Window creation failed\r\n",
-			"Window Creation Failed",
-			MB_ICONERROR);
-	}
-
-	ShowWindow(hWnd, nShowCmd);
-
-	MSG msg;
-	ZeroMemory(&msg, sizeof(MSG));
-
-	while (GetMessage(&msg, NULL, 0, 0))
-	{
-		TranslateMessage(&msg);
-		DispatchMessage(&msg);
-	}
-
-	return 0;
-}
-LRESULT CALLBACK WinProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+const char g_szClassName[] = "myWindowClass";
+HWND textfield, button, textbox;
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
 	switch (msg)
 	{
+	case WM_CLOSE:
+		DestroyWindow(hwnd);
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hwnd, msg, wParam, lParam);
 	case WM_CREATE:
-	{
-		// Create an edit box
-		hEdit = CreateWindowEx(WS_EX_CLIENTEDGE,
-			"EDIT",
-			"",
-			WS_CHILD | WS_VISIBLE |
-			ES_MULTILINE | ES_AUTOVSCROLL | ES_AUTOHSCROLL,
-			50,
-			100,
-			200,
-			100,
-			hWnd,
-			(HMENU)IDC_MAIN_EDIT,
-			GetModuleHandle(NULL),
-			NULL);
-		HGDIOBJ hfDefault = GetStockObject(DEFAULT_GUI_FONT);
-		SendMessage(hEdit,
-			WM_SETFONT,
-			(WPARAM)hfDefault,
-			MAKELPARAM(FALSE, 0));
-		SendMessage(hEdit,
-			WM_SETTEXT,
-			NULL,
-			(LPARAM)"");
-
-		// Create a push button
-		HWND hWndButton = CreateWindowEx(NULL,
-			"BUTTON",
-			"OK",
-			WS_TABSTOP | WS_VISIBLE |
-			WS_CHILD | BS_DEFPUSHBUTTON,
-			50,
-			220,
-			100,
-			24,
-			hWnd,
-			(HMENU)IDC_MAIN_BUTTON,
-			GetModuleHandle(NULL),
-			NULL);
-		SendMessage(hWndButton,
-			WM_SETFONT,
-			(WPARAM)hfDefault,
-			MAKELPARAM(FALSE, 0));
-	}
-	break;
-
+		textfield = CreateWindow("STATIC", "Carte de telefon", WS_VISIBLE | WS_CHILD, 80, 30, 225, 25, hwnd, NULL, NULL, NULL);
+		button = CreateWindow("BUTTON", "Modificati contacte", WS_VISIBLE | WS_CHILD | WS_BORDER, 25, 70, 140, 20, hwnd, NULL, NULL, NULL);
+		textbox = CreateWindow("EDIT", "Introduceti nodurile", WS_BORDER | WS_CHILD | WS_VISIBLE, 25, 100, 140, 20, hwnd, NULL, NULL, NULL);
+		button = CreateWindow("BUTTON", "Ruleaza", WS_VISIBLE | WS_CHILD | WS_BORDER, 220, 80, 80, 25, hwnd, (HMENU)1, NULL, NULL);
+		break;
 	case WM_COMMAND:
+
+	{
 		switch (LOWORD(wParam))
 		{
-		case IDC_MAIN_BUTTON:
+		case 1:
 		{
-			char buffer[256];
-			SendMessage(hEdit,
-				WM_GETTEXT,
-				sizeof(buffer) / sizeof(buffer[0]),
-				reinterpret_cast<LPARAM>(buffer));
-			MessageBox(NULL,
-				buffer,
-				"Information",
-				MB_ICONINFORMATION);
+			MessageBox(NULL, "Eroare", "Loading..", MB_OK | MB_ICONINFORMATION);
+			break;
 		}
-		break;
 		}
-		break;
-
-	case WM_DESTROY:
+	}
+	return 0;
+	}
+}
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+	LPSTR lpCmdLine, int nCmdShow)
+{
+	WNDCLASSEX wc;
+	HWND hwnd;
+	MSG Msg;
+	//Step 1: Registering the Window Class
+	wc.cbSize = sizeof(WNDCLASSEX);
+	wc.style = 0;
+	wc.lpfnWndProc = WndProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = hInstance;
+	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wc.lpszMenuName = NULL;
+	wc.lpszClassName = g_szClassName;
+	wc.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+	if (!RegisterClassEx(&wc))
 	{
-		PostQuitMessage(0);
+		MessageBox(NULL, "Window Registration Failed!", "Error!",
+			MB_ICONEXCLAMATION | MB_OK);
 		return 0;
 	}
-	break;
+	// Step 2: Creating the Window
+	hwnd = CreateWindowEx(
+		WS_EX_CLIENTEDGE,
+		g_szClassName,
+		"Proiect TP",
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT, 360, 190,
+		NULL, NULL, hInstance, NULL);
+	if (hwnd == NULL)
+	{
+		MessageBox(NULL, "Window Creation Failed!", "Error!",
+			MB_ICONEXCLAMATION | MB_OK);
+		return 0;
 	}
-
-	return DefWindowProc(hWnd, msg, wParam, lParam);
+	ShowWindow(hwnd, nCmdShow);
+	UpdateWindow(hwnd);
+	while (GetMessage(&Msg, NULL, 0, 0) > 0)
+	{
+		TranslateMessage(&Msg);
+		DispatchMessage(&Msg);
+	}
+	return Msg.wParam;
 }
